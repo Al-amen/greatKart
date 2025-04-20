f81c67644d004aeae50da86e1da9dbf830b8deda
Al-Amen <alamen15@cse.pstu.ac.bd>
2025-04-20 11:28:25 +0600

    create carts app and cart, cartItem model

---
+++ carts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Cart, CartItem
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id', 'date_added')
+    search_fields = ('cart_id',)
+    list_per_page = 10
+admin.site.register(Cart, CartAdmin)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'is_active')
+    search_fields = ('product',)
+    list_per_page = 10
+admin.site.register(CartItem, CartItemAdmin)
