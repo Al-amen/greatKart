80e23358fdb52b57c9e91092a3f69473b17a6ab0
Al-Amen <alamen15@cse.pstu.ac.bd>
2025-04-20 22:45:01 +0600

    fixed empty cart problem

--- carts/urls.py
+++ carts/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.cart, name='cart'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'), # for function based view
     #path('add/<int:product_id>/', views.AddToCartView.as_view(), name='add_to_cart'), # for class based view
+    path('remove/<int:product_id>/', views.remove_cart_quantity, name='remove_cart_quantity'), # for function based view
+    path('remove-cart-item<int:product_id>/', views.remove_cart_item, name='remove_cart_item'), # for function based view
+    
+
 ]
 
--- carts/views.py
+++ carts/views.py
@@ -27,6 +27,28 @@ def add_to_cart(request,product_id):
         cart_item.save()
     return  redirect('carts:cart')
 
+
+def remove_cart_quantity(request,product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except CartItem.DoesNotExist:
+        pass
+    return redirect('carts:cart')
+
+def remove_cart_item(request,product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('carts:cart')
+
 class AddToCartView(View):
     def get_cart_id(self, request):
         cart_id = request.session.session_key
@@ -63,7 +85,7 @@ def cart(request,total=0,quantity=0, cart_items=None):
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart,is_active=True)
         for cart_item in cart_items:
-            total = (cart_item.product.price * cart_item.quantity)
+            total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
     except Cart.DoesNotExist:
         cart_items = []
