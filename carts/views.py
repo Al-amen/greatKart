80e252828a3648b24c3011fe79fd6375985429fb
Al-Amen <alamen15@cse.pstu.ac.bd>
2025-04-22 17:59:13 +0600

    increment quantity decrement quantity and remove cart item has been fixed

--- carts/views.py
+++ carts/views.py
@@ -70,11 +70,11 @@ def add_to_cart(request,product_id):
     return redirect('carts:cart')
 
 
-def remove_cart_quantity(request,product_id):
+def remove_cart_quantity(request,product_id,cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
         if cart_item.quantity > 1:
             cart_item.quantity -= 1
             cart_item.save()
@@ -84,10 +84,10 @@ def remove_cart_quantity(request,product_id):
         pass
     return redirect('carts:cart')
 
-def remove_cart_item(request,product_id):
+def remove_cart_item(request,product_id,cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
     cart_item.delete()
     return redirect('carts:cart')
 
